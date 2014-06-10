6706fc0e404c3dd5f7d9ba544020f0d0e3acbe41
Nikolay <nykerrr@users.noreply.github.com>
2014-06-10 18:00:12 +0300

    Update _02_letterWordGenerator.java

--- _02_letterWordGenerator.java
+++ _02_letterWordGenerator.java
@@ -9,7 +9,7 @@ public class _02_Generate3LetterWords {
   	for (int i = 0; i < arr.length; i++) {
 	  	for (int j = 0; j < arr.length; j++) {
 		  	for (int k = 0; k < arr.length; k++) {
-  				System.out.printf("%s%s%s ", arr[i],arr[j],arr[k]);
+  				System.out.printf("%s%s%s ",arr[i],arr[j],arr[k]);
   			}
 		}
 	 }
